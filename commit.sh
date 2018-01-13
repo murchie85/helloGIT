3e8c353f793bcb265ef5f09f3def7a1a9c97045c
Adam <murchie85@gmail.com>
2018-01-13 21:36:26 +0000

    howdy

--- commit.sh
+++ commit.sh
@@ -1,9 +1,13 @@
+#simple shell script to auto commit
+
+$commitMessage
+
 git add .
 
 echo 'Enter the commit message:'
-read commitMessage
+#read commitMessage
 
-git commit -m "$commitMessage"
+git commit -m "$1"
 
 #echo 'Enter the name of the branch:'
 #read branch
